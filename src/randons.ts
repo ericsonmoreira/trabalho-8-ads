aa67e95ce7cc5394a2582d3ae66b53d97c4d0659
ericsonmoreira <moreira.ericson@gmail.com>
2021-01-31 18:51:08 -0300

    adjusts to add seedrandom

--- src/randons.ts
+++ src/randons.ts
@@ -1,5 +1,8 @@
 import { Point } from './types';
+import seedrandom from 'seedrandom';
+
+const random = seedrandom('seed para testar');
 
 export const getRandomPoint = (): Point => {
-  return { x: Math.random() * 2 - 1, y: Math.random() * 2 - 1 };
+  return { x: random() * 2 - 1, y: random() * 2 - 1 };
 };
