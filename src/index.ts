b2e0cd00fc774ef371f1aaa36d6eb9ba0a611560
ericsonmoreira <moreira.ericson@gmail.com>
2021-01-31 15:52:03 -0300

    adjust

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import simulation from './simulation';
 
-const valuesSimumaltions = [50, 100, 1000, 10000];
+const valuesSimumaltions = [50, 100, 1000, 100000];
 
 valuesSimumaltions.forEach((total) => simulation(total));
